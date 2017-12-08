0fd19c7c4ce6189e0d379231ec249511d72fe3f0
Alex Mekelburg <alex@mekware.com>
2017-12-08 09:17:46 -0500

    add welcome message

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :voter_record_updates
 
   after_create :get_current_voter_record
-  
+  after_create :send_welcome_notification
   
   def get_current_voter_record
     # When hooked up to state systems, this will pull an initial record from those systems
@@ -44,25 +44,50 @@ class User < ApplicationRecord
     new_vr.save!
   end
   
-  def notify(vr_update)
-    notification_url = Rails.application.routes.url_helpers.voter_record_update_url(vr_update)
-    
-    twilio_sid = ENV['TWILIO_SID']
-    twilio_token = ENV["TWILIO_TOKEN"]
-    twilio_phone_number = ENV['TWILIO_NUMBER']
-
-    number_to_send_to = self.phone
-
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-
-    @twilio_client.api.account.messages.create(
-        :from => "+1#{twilio_phone_number}",
-        :to => number_to_send_to,
-        :body => "We detected a change to your voter record. For more details visit #{notification_url}"
-    )
-    
-    
+  def send_welcome_notification
+    if true || self.notification_type.to_sym == :sms
+      twilio_client.api.account.messages.create(
+          :from => "+1#{twilio_phone_number}",
+          :to => sms_number,
+          :body => I18n.t('messages.welcome.sms')
+      )
+    else
+      # send email
+    end
   end
   
+  def notify(vr_update)
+    notification_url = Rails.application.routes.url_helpers.voter_record_update_url(vr_update)
+    if true || self.notification_type.to_sym == :sms
+      twilio_client.api.account.messages.create(
+          :from => "+1#{twilio_phone_number}",
+          :to => sms_number,
+          :body => I18n.t('messages.update.sms', notification_url: notification_url)
+      )
+    else
+      # send email
+    end
+  end
+
+  def sms_number
+    self.phone.to_s.gsub(/[^\d]/, '')
+  end
+  
+  def twilio_client
+    @twilio_client ||= Twilio::REST::Client.new twilio_sid, twilio_token      
+  end
+  
+  def twilio_sid
+    @twilio_sid ||= ENV['TWILIO_SID']
+  end
+  
+  def twilio_token
+    @twilio_token ||= ENV["TWILIO_TOKEN"]
+  end
+  
+  def twilio_phone_number 
+    @twilio_phone_number ||= ENV['TWILIO_NUMBER']
+  end
+
   
 end
