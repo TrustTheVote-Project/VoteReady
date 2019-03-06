2ddcb7a50ed589222cd5ece759e91b831d34abc9
Alex Mekelburg <alex@mekware.com>
2019-03-06 14:48:25 -0500

    redirect to ttv

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  before_action :redirect_to_ttv
+  
   protected
+  def redirect_to_ttv
+    redirect_to "https://trustthevote.org/voteready/"
+  end
+  
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
