32f81cbda5da8898cc1321eec34406ccfd46abca
Alex Mekelburg <alex@mekware.com>
2019-06-19 16:09:23 -0400

    update code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   protected
   def redirect_to_ttv
-    redirect_to "https://trustthevote.org/voteready/" unless request.host.contains?("demo.voteready.org")
+    redirect_to "https://trustthevote.org/voteready/" unless request.host.includes?("demo.voteready.org")
   end
   
   def after_sign_in_path_for(resource)
